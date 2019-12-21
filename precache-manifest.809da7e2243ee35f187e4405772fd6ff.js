17f14c12907634179f365db613974fae3a3d76c8
ipioo <ipioo-net@qq.com>
2019-12-21 19:40:33 +0800

    update index1

---
+++ precache-manifest.809da7e2243ee35f187e4405772fd6ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10073612609dda830eae6d0ca3f78d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b7fe95d30c86856811",
+    "url": "/static/js/2.b2d52a48.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b2d52a48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8b99fe629fa389b055f",
+    "url": "/static/js/main.5ab9e728.chunk.js"
+  },
+  {
+    "revision": "be143e83d2350e0e66b2",
+    "url": "/static/js/runtime-main.ac995be5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd4cd3907afbdd81cd0105efab231fe0.js"
+  "/precache-manifest.809da7e2243ee35f187e4405772fd6ff.js"
 );
 
 self.addEventListener('message', (event) => {
